83d696628a37c68ee5b911fc7b85004a05cfe1dd
Ruairi <ruairiboyd@gmail.com>
2024-06-19 20:15:30 +0100

    Initial Commit

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a simple example of a React component rendered with Vite.</p>
+    </>
+  );
+}
