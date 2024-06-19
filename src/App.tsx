b20e9aa4f6a5aac2781bf34c0d008e17ecdeb035
Ruairi <ruairiboyd@gmail.com>
2024-06-19 20:21:01 +0100

    chore: Update page title and navbar in index.html and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import HelloWorld from '@components/HelloWorld';
-
 function App() {
   return (
-    <>
-      <HelloWorld />
-    </>
+    <div className='app-container'>
+      <div className='container-fluid p-0'>
+        <nav className='navbar primary'>
+          <div className='container-fluid'>
+            <div className='navbar-brand'>
+              <h2 className='fs-2 fw-bold'>Alliance DevOps Dashboard</h2>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className='container-fluid p-0 m-0'>
+        <nav className='navbar light align-items-center border border-1'>
+          <div className='container-fluid justify-content-between'>
+            Breadcrumbs
+            <div className='row'>
+              <div className='col'>Teams</div>
+              <div className='col'>Release</div>
+              <div className='col'>Time</div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className='container-fluid p-0 content-container'>
+        {/* An <Outlet> renders whatever child route is currently active */}
+        <div className='content'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          imperdiet libero eu neque facilisis.
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/HelloWorld.tsx
+++
@@ -1,8 +0,0 @@
-export default function HelloWorld() {
-  return (
-    <>
-      <h1>Hello World</h1>
-      <p>This is a simple example of a React component rendered with Vite.</p>
-    </>
-  );
-}
